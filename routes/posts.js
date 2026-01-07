899421d267c85aa7fb7b259078c9a08345b48437
kelvin akunwa <kelvinakunwa101@gmail.com>
2026-01-07 11:58:27 +0100

    Fix posts route export crash

--- routes/posts.js
+++ routes/posts.js
@@ -4,17 +4,20 @@ const auth = require('../middleware/auth');
 const Post = require('../models/Posts');
 
 router.post('/', auth, async (req, res) => {
-   try {
-     const post = new Post ({ text: req.body.text, user: req.user.id });
-     await post.save();
-     res.json(post);
-   } catch (err) {
-     console.error(err.message);
-     res.status(500).send('Server error');
-   }
+  try {
+    const post = new Post({
+      text: req.body.text,
+      user: req.user.id,
+    });
+
+    await post.save();
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
 });
-  
-  
+
 router.get('/', auth, async (req, res) => {
   try {
     const posts = await Post.find().sort({ date: -1 });
@@ -22,7 +25,7 @@ router.get('/', auth, async (req, res) => {
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server error');
-  }  
+  }
 });
 
-Module.exports = router; 
+module.exports = router;
