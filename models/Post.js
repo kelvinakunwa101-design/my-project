9a82b357f145b90c5a0451d275b70b6529b90c20
kelvin akunwa <kelvinakunwa101@gmail.com>
2026-01-07 12:20:30 +0100

    finalizing 2026 Bento Grid and Domain.

--- models/Post.js
+++ models/Post.js
@@ -1,7 +1,18 @@
 const mongoose = require('mongoose');
+
 const PostSchema = new mongoose.Schema({
-  text: { type: String, required: true },
-  user: { type: mongoose.Schema. Types.ObjectId, ref: 'User' },
-  date: { type: Date, default: Date.now }
+  text: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
-Module.exports = mongoose.model('Post', PostSchema);  
+
+module.exports = mongoose.model('Post', PostSchema);
--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
-const Post = require('../models/Posts');
+const Post = require('../models/Post');
 
 router.post('/', auth, async (req, res) => {
   try {
