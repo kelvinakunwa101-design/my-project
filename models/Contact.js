244bad907ccde6260ec1d01551ddc2d38d7aec9a
kelvin akunwa <kelvinakunwa101@gmail.com>
2026-01-10 12:29:50 +0100

    Add Contact model

---
+++ models/Contact.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const ContactSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    message: {
+      type: String,
+      required: true,
+      trim: true
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports =
+  mongoose.models.Contact || mongoose.model('Contact', ContactSchema);
